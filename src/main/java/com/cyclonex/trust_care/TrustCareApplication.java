2ec47d9ef3d8eef7f3a2dadded021c8ce8809130
Lakshan <lakshan.imantha02@gmail.com>
2026-02-07 16:30:26 +0530

    Implemented the project structure in TrustCare application

---
+++ src/main/java/com/cyclonex/trust_care/TrustCareApplication.java
@@ -0,0 +1,13 @@
+package com.cyclonex.trust_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyclonex/trust_care/TrustCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyclonex.trust_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
